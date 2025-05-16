a258d4435814930326bd3598f08d99699b0d5802
olarte18 <snheider2224656@correo.uis.edu.co>
2025-05-16 09:39:29 -0500

    database

---
+++ myapp/Backend/database.js
@@ -0,0 +1,21 @@
+//config/database
+require('dotenv').config();
+const {Sequelize} = require('sequelize');
+
+//crear la conexión a la base de datos
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+    host: process.env.DB_HOST,
+    dialect: proccess.env.DB_DIALECT,
+    logging: false,
+});
+
+async function connectDatabase() {
+    try {
+        await sequelize.authenticate();
+        console.log('Conexión a la base de datos establecida correctamente.');
+    } catch (error) {
+        console.error('No se pudo conectar a la base de datos:', error);
+    }
+    return sequelize;
+}
+module.exports = {sequelize};

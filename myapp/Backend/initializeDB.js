a750662a6efeaa2b05088107c8555495659d7ca0
Alejandro Salazar Rincon <alejitosarin@gmail.com>
2025-05-16 12:54:03 -0500

    mejora DB

--- myapp/Backend/initializeDB.js
+++ myapp/Backend/initializeDB.js
@@ -4,15 +4,21 @@ const initDB = async () => {
     try {
         await sequelize.query('CREATE DATABASE IF NOT EXISTS prueba');
         await sequelize.query('USE prueba');
+
         await sequelize.query(`
             CREATE TABLE IF NOT EXISTS users (
                 nombre VARCHAR(255) NOT NULL,
                 correo VARCHAR(255) NOT NULL,
                 contraseña VARCHAR(255) NOT NULL,
-
                 PRIMARY KEY (correo)
             )`);
-        await sequelize.query(`INSERT INTO users (nombre, correo, contraseña) VALUES ('pruebas','prueba@gmail.com','prueba');`)
+
+        const [results] = await sequelize.query("SELECT COUNT(*) as count FROM users WHERE correo = 'prueba@gmail.com'");
+
+        if (results[0].count === 0) {
+            await sequelize.query(`INSERT INTO users (nombre, correo, contraseña) VALUES ('pruebas','prueba@gmail.com','prueba');`);
+        }
+
     } catch (error) {
         console.error('Error initializing the database:', error);
     } finally {

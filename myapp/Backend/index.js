146369b8509ac27eeb6a93ec22bcd361a7b1f1ca
olarte18 <snheider2224656@correo.uis.edu.co>
2025-05-16 09:26:57 -0500

    backend

---
+++ myapp/Backend/index.js
@@ -0,0 +1,11 @@
+const app = require('./app');
+const {initDB}=require('.initializeDB');
+require('./database');
+initDB();
+
+async function main(){
+    await app.listen(app.get('port'));
+    console.log('Server on port', app.get('port'));
+}
+
+main();
